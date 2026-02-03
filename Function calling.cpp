5bf2271ddd7d8eae6c52305e9913988edfb06618
6404062630490Panukorn <s6404062630490@email.kmutnb.ac.th>
2026-02-03 19:02:40 +0700

    Refactor function calls and add comments
    Updated function definitions and corrected comments regarding function calls.

--- Function calling.cpp
+++ Function calling.cpp
@@ -39,3 +39,13 @@ int functionN(int a, double b){
 	return 2, 5.1;
 }
 
+// b correct because [a = functionM] can call function but didnt return value
+// c wrong because [functionN(a,b)] is int and double but a,b is int
+// d correct brcause [functionO(r,a,s,t)] r,a,s,t is correct type
+// e wrong because [functionP(a,b,c,d,e)] call 5 value but functionP receive 4 value
+// f correct because this function return double and u value is double 
+// g wrong because [functionN(r,s)] value is int,double but r is int
+// h correct because [functionO(r,a,r,a)] is correct type
+// i wrong because [functionP(r,s,t,t)] is all double type but functionP is all int
+// j wrong because [functionP(functionN(a,a),s,t,t+r)] functionN the second a should be double type and functionP should be all int type value
+// k wrong because [functionP(functionN(a, a), s, t, t+r)] same with question j
